44a814898a646c8032a57ae41872c2af37c012c0
Srikant Subramanian <srikantvenkatsubramanian@gmail.com>
2024-10-30 22:46:41 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,33 @@
+from flask import Flask,render_template,request
+app=Flask(__name__)
+GOOGLE_API_KEY="AIzaSyDHwUe9wt4l2XQvYfVrQV6JoAUzIeiN4k4"
+import google.generativeai as genai
+genai.configure(api_key=GOOGLE_API_KEY)
+generation_config = {
+  "temperature": 0.9,
+  "top_p": 1,
+  "max_output_tokens": 2048,
+  "response_mime_type": "text/plain",
+}
+
+model = genai.GenerativeModel(
+  model_name="gemini-1.0-pro-001",
+  generation_config=generation_config,
+)
+
+@app.route("/",methods=["GET","POST"])
+def k():
+    if request.method=="POST":
+        a = request.form.get("verb-1")
+        b = request.form.get("verb-2")
+        c = request.form.get("adjective-1")
+        d = request.form.get("noun-1")
+        a1 = model.generate_content(f"generate a story based on four words {a}, {b}, {c}, {d} send it in innerhtml format with <span> around every input")
+        print(a1.text)
+        return render_template("index2.html",data=a1.text, shit="i hate this shit fr")
+
+
+    return render_template("index.html")
+
+
+app.run()
